1f3890069aa9dfc1346a94ac654518a46523b071
treesea2014 <2117999344@qq.com>
2016-03-11 10:34:09 +0800

    提交

--- src/test/java/com/kkb/test/pages/pc/gxb/LoginPage.java
+++ src/test/java/com/kkb/test/pages/pc/gxb/LoginPage.java
@@ -19,26 +19,28 @@ public class LoginPage extends Page {
 
 	@FindBy(linkText = "登录/注册")
 	public WebElement tologinBtn;
-	//学号登录
-	@FindBy(id = "studentId")
-	public WebElement studentId;
 
-	
-	@FindBy(xpath = "(//*[@id='username'])[2]")
-	public WebElement username;
-
-	@FindBy(xpath = "(//*[@id='password'])[2]")
-	public WebElement password;
-
-	/**
-	 * 登陆按钮
-	 */
-	@FindBy(xpath = "(//input[@name='submit'])[2]")
-	public WebElement loginBtn;
-	
 	/**
 	 * 用户信息 用于判断是否登录
 	 */
 	@FindBy(xpath = "//div[@id='userInfo']/a")
 	public WebElement loginInfo;
+
+	@FindBy(xpath = "//div[@class='wrapper']/div[2]/div[@class='mainRight']/ul/li[contains(text(), '学号登录')]")
+	public WebElement studentId;
+
+
+	@FindBy(xpath = "(//*[@id='username'])[1]")
+	public WebElement username;
+
+	@FindBy(xpath = "(//*[@id='password'])[1]")
+	public WebElement password;
+
+	/**
+	 * 登陆按钮
+	 */
+	@FindBy(xpath = "(//input[@name='submit'])[1]")
+	public WebElement loginBtn;
+
+
 }
