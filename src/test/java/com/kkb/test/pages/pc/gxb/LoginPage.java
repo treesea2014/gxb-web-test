d2f2889657e2669760faabf719ebd1f55f145489
wxxu <wxxu@gaoxiaobang.com>
2016-03-14 10:18:13 +0800

    修改登录页面

--- src/test/java/com/kkb/test/pages/pc/gxb/LoginPage.java
+++ src/test/java/com/kkb/test/pages/pc/gxb/LoginPage.java
@@ -13,34 +13,35 @@ import com.kkb.test.pages.Page;
  */
 public class LoginPage extends Page {
 
-	public LoginPage(WebDriver webDriver) {
-		super(webDriver);
-	}
+    public LoginPage(WebDriver webDriver) {
+        super(webDriver);
+    }
 
-	@FindBy(linkText = "登录/注册")
-	public WebElement tologinBtn;
+    @FindBy(linkText = "登录/注册")
+    public WebElement tologinBtn;
 
-	/**
-	 * 用户信息 用于判断是否登录
-	 */
-	@FindBy(xpath = "//div[@id='userInfo']/a")
-	public WebElement loginInfo;
+    /**
+     * 用户信息 用于判断是否登录
+     */
+    @FindBy(xpath = "//div[@id='userInfo']/a")
+    public WebElement loginInfo;
 
-	@FindBy(xpath = "//div[@class='wrapper']/div[2]/div[@class='mainRight']/ul/li[contains(text(), '学号登录')]")
-	public WebElement studentId;
+    //	@FindBy(xpath = "//div[@class='wrapper']/div[2]/div[@class='mainRight']/ul/li[contains(text(), '学号登录')]")
+    @FindBy(xpath = "//div[@class='wrapper']/div[2]/div[@class='mainRight']/ul/li[contains(text(), '账号登录')]")
+    public WebElement studentId;
 
 
-	@FindBy(xpath = "(//*[@id='username'])[1]")
-	public WebElement username;
+    @FindBy(xpath = "(//*[@id='username'])[2]")
+    public WebElement username;
 
-	@FindBy(xpath = "(//*[@id='password'])[1]")
-	public WebElement password;
+    @FindBy(xpath = "(//*[@id='password'])[2]")
+    public WebElement password;
 
-	/**
-	 * 登陆按钮
-	 */
-	@FindBy(xpath = "(//input[@name='submit'])[1]")
-	public WebElement loginBtn;
+    /**
+     * 登陆按钮
+     */
+    @FindBy(xpath = "(//input[@name='submit'])[2]")
+    public WebElement loginBtn;
 
 
 }
