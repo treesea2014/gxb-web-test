12f4c700e8290738445c2cfbae6eba30457ea545
wxxu <wxxu@gaoxiaobang.com>
2016-04-22 18:52:04 +0800

    登录

--- src/test/java/com/kkb/test/pages/pc/gxb/gcu/LoginPage.java
+++ src/test/java/com/kkb/test/pages/pc/gxb/gcu/LoginPage.java
@@ -20,17 +20,30 @@ public class LoginPage extends Page {
 	@FindBy(linkText = "登录/注册")
 	public WebElement tologinBtn;
 	
-	@FindBy(name = "j_username")
+//	@FindBy(name = "j_username")
+//	public WebElement username;
+//
+//	@FindBy(name = "j_password")
+//	public WebElement password;
+//
+//	/**
+//	 * 登陆按钮
+//	 */
+//	@FindBy(xpath = "//button[text()='登录']")
+//	public WebElement loginBtn;
+
+	@FindBy(xpath = "//*[@id='IDToken1']")
 	public WebElement username;
 
-	@FindBy(name = "j_password")
+	@FindBy(xpath = "//*[@id='IDToken2']")
 	public WebElement password;
 
 	/**
 	 * 登陆按钮
 	 */
-	@FindBy(xpath = "//button[text()='登录']")
+	@FindBy(xpath = "//*[@id='my_td']/table/tbody/tr[5]/td[2]/a/img")
 	public WebElement loginBtn;
+
 
 	/**
 	 * 用户信息 用于判断是否登录
