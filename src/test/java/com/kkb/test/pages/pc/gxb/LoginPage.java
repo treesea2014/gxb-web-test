197138e9fa939a5b7886d7e5bfdf8c6b9a256d54
wxxu <wxxu@gaoxiaobang.com>
2016-04-27 13:15:24 +0800

    账号

--- src/test/java/com/kkb/test/pages/pc/gxb/LoginPage.java
+++ src/test/java/com/kkb/test/pages/pc/gxb/LoginPage.java
@@ -26,21 +26,20 @@ public class LoginPage extends Page {
     @FindBy(xpath = "//div[@id='userInfo']/a")
     public WebElement loginInfo;
 
-    @FindBy(xpath = "//div[@class='wrapper']/div[2]/div[@class='mainRight']/ul/li[contains(text(), '学号登录')]")
+    @FindBy(xpath = "//div[@class='wrapper']/div[2]/div[@class='mainRight']/ul/li[contains(text(), '账号登录')]")
     public WebElement studentId;
 
 
-    @FindBy(xpath = "(//*[@id='username'])[1]")
+    @FindBy(xpath = "(//*[@id='username'])[2]")
     public WebElement username;
 
-    @FindBy(xpath = "(//*[@id='password'])[1]")
+    @FindBy(xpath = "(//*[@id='password'])[2]")
     public WebElement password;
 
     /**
      * 登陆按钮
      */
-    @FindBy(xpath = "(//input[@name='submit'])[1]")
+    @FindBy(xpath = "(//input[@name='submit'])[2]")
     public WebElement loginBtn;
-
 
 }
